a5452af47f17a5c34996d72724aae768738ddd96
Regis Digital <regissdigital@gmail.com>
2026-06-08 04:47:26 +0530

    Update JS map logic to plot points using Nominatim

--- transport_management_system/transport_management_system/doctype/tms_route/tms_route.js
+++ transport_management_system/transport_management_system/doctype/tms_route/tms_route.js
@@ -12,7 +12,6 @@ frappe.ui.form.on('TMS Route', {
     },
     
     render_map: function(frm) {
-        // Find or create a map container above the stops table
         let $wrapper = frm.get_field('stops').$wrapper;
         let map_id = 'leaflet-map-' + frm.doc.name;
         
@@ -20,7 +19,6 @@ frappe.ui.form.on('TMS Route', {
             $('<div id="' + map_id + '" style="height: 400px; width: 100%; border: 1px solid #d1d8dd; border-radius: 4px; margin-bottom: 15px; z-index: 1;"></div>').insertBefore($wrapper.find('.grid-wrapper'));
         }
         
-        // Load Leaflet dynamically if not loaded
         frappe.require([
             'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
             'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
@@ -29,49 +27,42 @@ frappe.ui.form.on('TMS Route', {
                 frm.map_instance.remove();
             }
             
-            // Initialize map
-            frm.map_instance = L.map(map_id).setView([0, 0], 2);
+            frm.map_instance = L.map(map_id).setView([20.5937, 78.9629], 5); // Default center India
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; OpenStreetMap contributors'
             }).addTo(frm.map_instance);
             
-            // Collect stops and geocode them (Simulated using Frappe's built-in geocoding or basic search)
             let bounds = [];
             
-            frm.doc.stops.forEach(function(stop, index) {
-                if (stop.location) {
-                    // For a real production app, we would use a geocoding API or a custom coordinate field.
-                    // Since Frappe locations are usually addresses, we'll fetch coordinates if available in an Address doctype
-                    // Here we will just simulate plotting if we had coords, or use a basic search.
-                    
-                    frappe.call({
-                        method: 'frappe.geo.utils.get_coords',
-                        args: { name: stop.location }, // Assume location is an Address name
-                        callback: function(r) {
-                            if (r.message) {
-                                let lat = r.message.latitude;
-                                let lon = r.message.longitude;
-                                if (lat && lon) {
-                                    let marker = L.marker([lat, lon]).addTo(frm.map_instance);
-                                    marker.bindPopup(`<b>Stop ${stop.stop_sequence}</b><br>${stop.location}<br>${stop.stop_type}`);
-                                    bounds.push([lat, lon]);
-                                    frm.map_instance.fitBounds(bounds);
-                                }
-                            }
-                        }
-                    });
-                }
+            let locations_to_plot = [];
+            if (frm.doc.start_location) locations_to_plot.push({loc: frm.doc.start_location, title: 'Start: ' + frm.doc.start_location});
+            
+            frm.doc.stops.forEach(function(stop) {
+                if (stop.location) locations_to_plot.push({loc: stop.location, title: 'Stop ' + stop.stop_sequence + ': ' + stop.location});
             });
             
-            // Example default fallback coordinates if geocoding is slow or empty (just for visual representation)
-            if (bounds.length === 0) {
-                // Try some dummy coordinates if none exist, so the user sees *something*
-                let d_lat = 40.7128 + (Math.random() * 0.1);
-                let d_lon = -74.0060 + (Math.random() * 0.1);
-                let m1 = L.marker([d_lat, d_lon]).addTo(frm.map_instance);
-                m1.bindPopup("Demo Stop 1");
-                frm.map_instance.setView([d_lat, d_lon], 10);
-            }
+            if (frm.doc.end_location) locations_to_plot.push({loc: frm.doc.end_location, title: 'End: ' + frm.doc.end_location});
+
+            locations_to_plot.forEach(function(item) {
+                frappe.call({
+                    method: 'geocode_location',
+                    doc: frm.doc,
+                    args: { location_name: item.loc },
+                    callback: function(r) {
+                        if (r.message) {
+                            let parts = r.message.split(',');
+                            let lon = parseFloat(parts[0]);
+                            let lat = parseFloat(parts[1]);
+                            if (lat && lon) {
+                                let marker = L.marker([lat, lon]).addTo(frm.map_instance);
+                                marker.bindPopup(`<b>${item.title}</b>`);
+                                bounds.push([lat, lon]);
+                                frm.map_instance.fitBounds(bounds, {padding: [50, 50]});
+                            }
+                        }
+                    }
+                });
+            });
         });
     }
 });
